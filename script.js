9ff7d61b633f86f7b4d41b7923c27085958afc8d
SimoneC-1994 <simone.curcio94@gmail.com>
2022-01-26 19:34:11 +0100

    finish

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (var name in names) {
   var firstLetter = names[name].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
-    GobyeSpeaker.speak(names[name]);
+    byeSpeaker.speak(names[name]);
   } 
   else {
-    HelloSpeaker.speak(names[name]);
+    helloSpeaker.speak(names[name]);
   }
 }
 })();
